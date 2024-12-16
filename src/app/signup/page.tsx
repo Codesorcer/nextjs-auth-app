ab97596432c35c35f5013b73309bcdd82db0ee2b
Codesorcer <Rudraprataps7789@gmail.com>
2024-12-16 18:39:28 +0530

    add verify email feature

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import Link from "next/link";
 import React, { useEffect } from "react";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 import axios from "axios";
-import {toast} from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 export default function SignupPage() {
   const router = useRouter();
@@ -11,34 +11,36 @@ export default function SignupPage() {
     email: "",
     password: "",
     username: "",
-  })
-
+  });
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
 
   const onSignup = async () => {
     try {
       setLoading(true);
-      const response = await axios.post("/api/users/signup", user)
-      console.log("Singnup success", response.data)
-      router.push("/login")
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");
     } catch (error: any) {
-      console.log("Signup failed", error.message)
+      console.log("Signup failed", error.message);
 
-      toast.error(error.message)
+      toast.error(error.message);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0){
-      setButtonDisabled(false)
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
     } else {
       setButtonDisabled(true);
     }
-  }, [user])
-
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -71,14 +73,13 @@ export default function SignupPage() {
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
       />
-
       <button
         onClick={onSignup}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
         {buttonDisabled ? "No signup" : "Signup"}
       </button>
-      <Link href="/login">Visit login page </Link>
+      <Link href="/login">Visit login page</Link>
     </div>
   );
 }
